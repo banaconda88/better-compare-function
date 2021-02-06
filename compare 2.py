33b037aded791e8ec4d78ad5fa821a3c56772c50
banaconda88 <bananaconda88@gmail.com>
2021-02-06 12:48:19 -0500

    Add files via upload

---
+++ compare 2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def compare():
+    x = input("What is the number you would like to be your X?")
+    y = input("What is the number you want to be your Y?")
+    z = input("What is the number you want to be your Z? ")
+    if x <= y:
+        if y <= z:
+            return True
+        else:
+            return False
+    else:
+        return False
+compare()
+
+
+# In[ ]:
+
+
+
+
